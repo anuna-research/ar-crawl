07020652a4c9500dca5106e2c26270598b2bd9bc
Hugo O'Connor <hugo@anuna.io>
2025-12-30 23:17:15 +1100

    Add package dependencies to info.rkt

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define collection "ar-crawl")
+(define deps '("base"
+               "html-parsing"
+               "uuid"
+               "gregor"
+               "http123"))
+(define build-deps '("rackunit-lib"))
